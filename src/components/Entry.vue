4454355cb30255eb4abfe321c96c384f9ff9f468
James Mare <git@jmare.me>
2018-05-04 16:38:56 +1000

    Added stub to show mapping and routing table

--- src/components/Entry.vue
+++ src/components/Entry.vue
@@ -3,6 +3,34 @@
     <h2>Links</h2>
     <links-view :links="links" @emitRefresh="emitRefresh"></links-view>
     <p v-if="links.length == 0">No links to display</p>
+    <br>
+    <h2>SYSID Mapping</h2>
+    <vue-bootstrap-table
+            :columns="columns"
+            :values="values"
+            :show-filter="e"
+            :show-column-picker="false"
+            :sortable="false"
+            :paginated="false"
+            :multi-column-sortable=false
+            :filter-case-sensitive=false
+
+    >
+    </vue-bootstrap-table>
+    <br>
+    <h2>Routing Table</h2>
+    <vue-bootstrap-table
+            :columns="columns2"
+            :values="values2"
+            :show-filter="e"
+            :show-column-picker="false"
+            :sortable="false"
+            :paginated="false"
+            :multi-column-sortable=false
+            :filter-case-sensitive=false
+
+    >
+    </vue-bootstrap-table>
     <b-button-group class="mngbtn">
       <b-button v-on:click="getLinks" variant="info">Refresh Links</b-button>
       <b-button v-b-modal.modaludp variant="primary">Add UDP</b-button>
@@ -14,19 +42,79 @@
 
   </div>
 </template>
-
 <script>
 
 import axios from 'axios'
+import VueBootstrapTable from 'vue2-bootstrap-table2'
 import LinksView from '@/components/LinksView'
 import AddLink from '@/components/AddLink'
 
 export default {
   name: 'Entry',
-  components: { LinksView, AddLink },
+  components: { LinksView, AddLink, VueBootstrapTable: VueBootstrapTable },
   data () {
     return {
-      links: []
+      links: [],
+      columns: [
+        {
+          title: 'Source',
+          visible: true,
+          editable: true
+        },
+        {
+          title: 'Dest',
+          visible: true,
+          editable: true
+        }
+      ],
+      columns2: [
+        {
+          title: 'Dest',
+          visible: true,
+          editable: true
+        },
+        {
+          title: 'Next Hop',
+          visible: true,
+          editable: true
+        }
+      ],
+      values: [
+        {
+          'Source': 1,
+          'Dest': 255
+        },
+        {
+          'Source': 255,
+          'Dest': 1
+        },
+        {
+          'Source': 2,
+          'Dest': 254
+        },
+        {
+          'Source': 245,
+          'Dest': 2
+        }
+      ],
+      values2: [
+        {
+          'Dest': 1,
+          'Next Hop': 0
+        },
+        {
+          'Dest': 2,
+          'Next Hop': 0
+        },
+        {
+          'Dest': 255,
+          'Next Hop': 1
+        },
+        {
+          'Dest': 254,
+          'Next Hop': 2
+        }
+      ]
     }
   },
   created: function () {
@@ -73,5 +161,3 @@ a {
     padding-top: 10px;
     }
 </style>
-
-    methods: {
